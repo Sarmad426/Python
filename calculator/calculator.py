19ee3d0b0cd4b0cce13cba741e42ca16ae12f94c
Sarmad rafique <sarmadrafique040@gmail.com>
2023-10-15 11:29:46 +0500

    Calculator code

---
+++ calculator/calculator.py
@@ -0,0 +1,25 @@
+class Calculator:
+    """Class representing a person"""
+
+    def __init__(self) -> None:
+        self.num1: int = int(input("Enter value 1:"))
+        self.num2: int = int(input("Enter value 2:"))
+
+    def add(self) -> int:
+        return self.num1+self.num2
+
+    def sub(self) -> int:
+        return self.num1-self.num2
+
+    def mul(self) -> int:
+        return self.num1*self.num2
+
+    def div(self) -> float:
+        return self.num1/self.num2
+
+
+calculator = Calculator()
+print(calculator.add())
+print(calculator.sub())
+print(calculator.mul())
+print(calculator.div())
