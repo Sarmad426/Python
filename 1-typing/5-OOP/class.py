6a12d897fb0696b69b8cb87580241b42a5ebda03
Sarmad rafique <sarmadrafique040@gmail.com>
2023-10-15 11:43:40 +0500

    Basic OOP Program

---
+++ 1-typing/5-OOP/class.py
@@ -0,0 +1,11 @@
+class Name:
+
+    def __init__(self):
+        self.name: str = str(input("Enter Your Name: "))
+
+    def greet(self):
+        return f"Welcome {self.name}"
+
+
+user = Name()
+print(user.greet())
