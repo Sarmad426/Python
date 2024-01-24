b6c6468b137c3037e9655b0691c6e67edfb28d77
Sarmad rafique <sarmadrafique040@gmail.com>
2024-01-24 13:08:06 +0500

    Reorganized the Repo structure

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,21 +1,21 @@
 class Calculator:
-    """Class representing a person"""
+    """Class representing a calculator object"""
 
     def __init__(self) -> None:
         self.num1: int = int(input("Enter value 1:"))
         self.num2: int = int(input("Enter value 2:"))
 
     def add(self) -> int:
-        return self.num1+self.num2
+        return self.num1 + self.num2
 
     def sub(self) -> int:
-        return self.num1-self.num2
+        return self.num1 - self.num2
 
     def mul(self) -> int:
-        return self.num1*self.num2
+        return self.num1 * self.num2
 
     def div(self) -> float:
-        return self.num1/self.num2
+        return self.num1 / self.num2
 
 
 calculator = Calculator()
