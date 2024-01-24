fa121d2b4ad4cf6278260226398ad18d7aef5bb4
Sarmad rafique <sarmadrafique040@gmail.com>
2024-01-24 22:59:45 +0500

    used functional programming

--- 4A-Exceptions/exceptions.py
+++ 4A-Exceptions/exceptions.py
@@ -1,10 +1,18 @@
 """Python Error Handling with try-except-else"""
 
-while True:
-    try:
-        num: int = int(input("Enter a number: "))
-        break
-    except ValueError:
-        print("You must enter a number!")
 
-print(num)
+def main() -> None:
+    num: int = get_num()
+    print(f"num: {num}")
+
+
+def get_num() -> int:
+    while True:
+        try:
+            num: int = int(input("Enter a number: "))
+            return num
+        except ValueError:
+            pass
+
+
+main()
