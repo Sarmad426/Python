cf0c8bfd671d3d8a94bdea24f68f2ff57a512fdb
Sarmad rafique <sarmadrafique040@gmail.com>
2024-01-26 22:27:48 +0500

    python conditions

---
+++ 2-Conditions/if-else.py
@@ -0,0 +1,10 @@
+"""Python Conditions if else"""
+
+num: int = int(input("Enter a number: "))
+
+if num == 0:
+    print("Number is zero")
+elif num > 0:
+    print("Number is greater than zero")
+else:
+    print("It's a negative number")
