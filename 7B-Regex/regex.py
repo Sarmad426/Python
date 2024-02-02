e1c637846611575ed0239b26c4cdc6fcdf3d4ea8
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-02 12:57:27 +0500

    Removed useless comments

--- 7B-Regex/regex.py
+++ 7B-Regex/regex.py
@@ -1,7 +1,12 @@
+"""
+Email Validator program using REGEX.
+"""
+
 import re
 
-# Example 3: Matching an email address
+# Matching an email address
+
 pattern = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
-email = str(input("Enter your email address: "))
-result = pattern.search(email)
-print(result.group())  # Output: support@example.com
+EMAIL: str = str(input("Enter your email address: "))
+result = pattern.search(EMAIL)
+print(result.group())
