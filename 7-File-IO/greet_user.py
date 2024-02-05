897cff51f49858887c540fd364b1f33ba4895aab
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-05 21:34:47 +0500

    Modified the code to standard

--- 7-File-IO/greet_user.py
+++ 7-File-IO/greet_user.py
@@ -3,13 +3,16 @@ Python program for file input-output.
 """
 
 import sys
+from typing import Callable
+
+get_input_name: Callable[[], str] = lambda: str(input("Name: ")).strip()
 
 
 def validate_input(name: str) -> str:
     """
     Validates user input name.
-    if input exist it returns it,
-    otherwise it exits the program
+
+    if input name exist, it returns the name, otherwise it exits the program with an error message
     """
     if not name:
         sys.exit("Name is required")
@@ -19,7 +22,9 @@ def validate_input(name: str) -> str:
 
 def write_file(name: str) -> None:
     """
-    function for writing a name into a file.
+    Writes a name into a file.
+
+    Takes one parameter `name` and returns nothing
     """
 
     with open("practice.txt", "w", encoding="utf-8") as file:
@@ -27,9 +32,15 @@ def write_file(name: str) -> None:
 
 
 # Reading the name from the file
-def read_file():
+
+
+def read_file() -> str:
     """
-    Reads the name from the file and greets the user
+    Reads the name from the file
+
+    Takes no parameters
+
+    returns the contents of the file
     """
     with open("practice.txt", "r", encoding="utf-8") as file:
         return file.read()
@@ -38,15 +49,19 @@ def read_file():
 def greet_user(name: str) -> None:
     """
     Greets the user
+
+    Parameters (1) name of type string \n
+    name : str
     """
     print(f"Welcome, {name}")
 
 
 def main():
     """
-    Main function to execute all the functions
+    Main function to execute all the functions.\n
+    It doesn't take any parameters neither returns anything
     """
-    name = str(input("Name: ")).strip()
+    name = get_input_name()
     validate_input(name)
     write_file(name)
     name = read_file()
