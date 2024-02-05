fa22d09aa6409b52bddb0689a981ad7af26f8f06
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-05 22:45:38 +0500

    Added module and function doc strings

--- 8-OOP/Excercises/calculator/calculator.py
+++ 8-OOP/Excercises/calculator/calculator.py
@@ -1,3 +1,8 @@
+"""
+Python class simple calculator program
+"""
+
+
 class Calculator:
     """Class representing a calculator object"""
 
@@ -6,15 +11,27 @@ class Calculator:
         self.num2: int = int(input("Enter value 2:"))
 
     def add(self) -> int:
+        """
+        Add two numbers and return them
+        """
         return self.num1 + self.num2
 
     def sub(self) -> int:
+        """
+        Subtract two numbers and return them
+        """
         return self.num1 - self.num2
 
     def mul(self) -> int:
+        """
+        Multiply two numbers and return them
+        """
         return self.num1 * self.num2
 
     def div(self) -> float:
+        """
+        Divide two numbers and return them
+        """
         return self.num1 / self.num2
 
 
