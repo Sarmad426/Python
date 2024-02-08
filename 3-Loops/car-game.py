a085dc29ee2ce99bc3d461f80da5e3bd2cbd4ed7
Muhammad Sarmad <sarmadrafique040@gmail.com>
2024-02-08 21:37:43 +0500

    Added module, class and function docstrings

--- 3-Loops/car-game.py
+++ 3-Loops/car-game.py
@@ -1,5 +1,11 @@
 """
 Python Car Game
+
+    - Start the car
+    - Stop the car
+    - Exit the game
+
+    Active start and stop states with user-friendly console messages.
 """
 
 import sys
@@ -8,6 +14,12 @@ import sys
 class CarGame:
     """
     Car Game class
+
+    - Start the car
+    - Stop the car
+    - Exit the game
+
+    Active start and stop states with user-friendly console messages.
     """
 
     commands: tuple[str, str, str, str] = ("start", "stop", "exit", "help")
@@ -17,7 +29,17 @@ class CarGame:
         self.started: bool = False
         self.stopped: bool = True
 
-    def start_game(self):
+    def start_game(self) -> None:
+        """
+        Starts the game
+
+        No Parameters:\n
+        Returns nothing:
+
+        Prompts the user to enter command infinitely
+
+        if command is valid it calls the function accordingly
+        """
         while True:
             command = input(":> ").strip()
             if command in CarGame.commands:
@@ -34,6 +56,14 @@ class CarGame:
                 self.help()
 
     def start_car(self, started: bool):
+        """
+        Starts the car by switching the states
+
+        Parameters:
+        - started (bool):
+
+        Returns Nothing
+        """
         if started:
             print("Car already started: ")
         else:
@@ -41,6 +71,14 @@ class CarGame:
             print("Car started: ")
 
     def stop_car(self, stopped: bool):
+        """
+        Stops the car by switching the states
+
+        Parameters:
+        - stopped (bool):
+
+        Returns Nothing
+        """
         if stopped:
             print("Car already stopped: ")
         else:
@@ -48,10 +86,19 @@ class CarGame:
             print("Car stopped: ")
 
     def help(self) -> None:
+        """
+        Prints the help message, list of commands
+        """
         print("start: to start the car\nstop: to stop the car\nexit: to exit the game")
 
 
 def main():
+    """
+    Main function
+
+    - Creates a `CarGame` class object
+    - starts the game
+    """
     car = CarGame()
 
     car.start_game()
