df2978f3049439d753667b41e2c65eb9523e21a4
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-08 17:51:55 +0500

    Standardized the code

--- 1-Data-types/5-dictionary-operations/digits_mapping.py
+++ 1-Data-types/5-dictionary-operations/digits_mapping.py
@@ -1,20 +1,55 @@
-characters: dict[str, str] = {
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four",
-    "5": "Five",
-    "6": "Six",
-    "7": "Seven",
-    "8": "Eight",
-    "9": "Nine",
-    "0": "Zero",
-}
+"""
+Digits mapping exercise
 
-output = ""
+Mapped phone number digits to english alphabets
 
-user_input = input("Phone Number: ")
+"""
 
-for ch in user_input:
-    output += characters.get(ch, "") + " "
-print(output)
+
+def map_digits(phone_num: str, data_set: dict[str, str]) -> str:
+    """
+    Maps Numeric digits to english alphabets
+
+    Parameters:
+    - phone_num (str): Phone number
+    - data_set (dict[str, str]) : Dictionary data set
+
+    Return type: str
+        returns the digit corresponding to the alphabets
+    """
+    mapped_digits = ""
+
+    for char in phone_num:
+        mapped_digits += data_set.get(char, "") + " "
+
+    return mapped_digits
+
+
+def main() -> None:
+    """
+    Main function
+
+    Initialized the Characters dictionary
+    """
+    characters: dict[str, str] = {
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        "0": "Zero",
+    }
+
+    PHONE_NUM = input("Phone Number: ")
+
+    output: str = map_digits(PHONE_NUM, characters)
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
