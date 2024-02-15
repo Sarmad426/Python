e9083df3512f24d1eaa36f599c94299359060817
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-15 22:39:21 +0500

    standardized the code

--- 5-Libraries/api_call.py
+++ 5-Libraries/api_call.py
@@ -4,16 +4,30 @@ Calls Github users api and stores the data in csv file
 
 import requests
 
+
+def get_github_user(username: str):
+    """
+    Gets the github user via api
+
+    Parameters:
+    username (str): Username of the github user
+
+    Returns:
+    user (json object): The github user's info as a json object. If not found, returns None
+    """
+    api_url = f"https://api.github.com/users/{github_user_search}"
+    response = requests.get(api_url)
+    user = response.json()
+    if response.status_code == 200:
+        return user
+    return None
+
+
 github_user_search = input("Search for Github user with username: ")
 
-api_url = f"https://api.github.com/users/{github_user_search}"
+user = get_github_user(github_user_search)
 
-
-response = requests.get(api_url)
-user = response.json()
-
-# Check if the response status code is 200 (OK)
-if response.status_code == 200:
+if user:
     print(user)
 else:
-    print("Error 401")
+    print("No GitHub user found!")
