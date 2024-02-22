fc35e42bdea7ad7dc80f3e08fd4f5ba0041e1077
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-22 15:50:06 +0500

    used function instead of procedural code

--- Exercices/fibonacci_series.py
+++ Exercices/fibonacci_series.py
@@ -4,11 +4,26 @@ Python Practice program
 
 # [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-total = 0
-count = 1
 
-while total < 100:
-    print(total)
-    prev = count
-    count = total
-    total += prev
+def fibonacci_series(limit: int) -> None:
+    """
+    Prints Fibonacci series (0,1,1,2,3,5,8,13)
+
+    Parameters:
+    `limit` (int) : limit of fibonacci series
+
+    Returns nothing
+    """
+    total = 0
+    count = 1
+
+    while total < limit:
+        print(total)
+        prev = count
+        count = total
+        total += prev
+
+
+if __name__ == "__main__":
+    LIMIT = int(input("Enter fibonacci series limit: "))
+    fibonacci_series(LIMIT)
