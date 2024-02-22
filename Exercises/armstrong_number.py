07c951ead6e0b32d297af83f38f764410493cd0b
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-22 20:10:49 +0500

    armstrong number checker

---
+++ Exercises/armstrong_number.py
@@ -0,0 +1,34 @@
+"""
+Program checks whether the input number is armstrong number or not.
+Armstrong numbers: $(153,371,407)$
+"""
+
+
+def check_armstrong(num: int) -> bool:
+    """
+    Checks whether the input number is armstrong number or not.
+
+    Parameters:
+    `num` (int) : number to check
+
+    Returns:
+    `bool` : True if the number is armstrong
+    """
+    power = len(str(num))
+    temp = num
+    total = 0
+    while temp >= 1:
+        digit = temp % 10
+        total += pow(digit, power)
+        temp = int(temp / 10)
+    if total == num:
+        return True
+    return False
+
+
+INPUT_NUM = int(input("Enter a number: "))
+
+if check_armstrong(INPUT_NUM):
+    print("Armstrong number")
+else:
+    print("Not an armstrong number")
