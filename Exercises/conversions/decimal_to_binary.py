3c75e2b5cc82257ef5bfcb31eb526bc7aef36d84
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-22 20:50:43 +0500

    Updated function import

--- Exercises/conversions/decimal_to_binary.py
+++ Exercises/conversions/decimal_to_binary.py
@@ -2,7 +2,7 @@
 Python program converts decimal input to binary
 """
 
-from helper_methods import reverse_string
+from reverse_string import reverse_string
 
 
 def decimal_to_binary(decimal: int) -> str:
