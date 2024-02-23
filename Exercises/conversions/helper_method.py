7baed24690a3d7e6b7798eeeef32551fe9b7af01
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-23 15:01:13 +0500

    helper method to validate binary input

--- Exercises/conversions/helper_method.py
+++ Exercises/conversions/helper_method.py
@@ -2,6 +2,8 @@
 Python custom helper functions
 """
 
+import re
+
 
 def reverse_string(string: str) -> str:
     """
@@ -14,6 +16,20 @@ def reverse_string(string: str) -> str:
         reverse = reverse + string[index]
         index -= 1
     return reverse
+
+
+def validate_binary(binary: str) -> bool:
+    """
+    Validates binary input via REGEX
+
+    Parameter:
+    `binary` (str) : binary value to validate
+
+    Returns:
+    bool : True if valid binary number
+    """
+    pattern = re.compile(r"^[01]+$")
+    return bool(pattern.match(binary))
 
 
 if __name__ == "__main__":
