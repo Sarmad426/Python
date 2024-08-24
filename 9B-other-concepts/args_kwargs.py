fa412837bc61035dc3ae4a57712c5fb84ad737ec
Sarmad rafique <sarmadrafique040@gmail.com>
2024-08-24 17:39:45 +0500

    Updated code and file name

--- 9B-other-concepts/args_kwargs.py
+++ 9B-other-concepts/args_kwargs.py
@@ -1,9 +1,18 @@
-from typing import Any
+"""
+Python args and keyword args
+"""
+
+from typing import Any, Dict
+
 
 # Positional Argument packing
 def add_numbers(*args: int) -> int:
+    """
+    Accepts any number of positional arguments and returns their sum.
+    """
     # *args allows the function to accept any number of positional arguments
     return sum(args)
+
 
 result = add_numbers(1, 2, 3, 4, 5)  # Passing multiple arguments
 print(result)
@@ -11,6 +20,9 @@ print(result)
 
 # Unpacking Iterables
 def greet(name1: str, name2: str, name3: str) -> None:
+    """
+    Greet three people by name.
+    """
     # Function expects three arguments
     print(f"Hello, {name1}, {name2}, and {name3}!")
 
@@ -21,6 +33,9 @@ greet(*names)  # Unpacking the list into separate arguments
 
 # Keyword argument packing
 def display_info(**kwargs: Any) -> None:
+    """
+    Display info using the `kwargs`
+    """
     # **kwargs allows the function to accept any number of keyword arguments
     for key, value in kwargs.items():
         print(f"{key}: {value}")
@@ -28,19 +43,31 @@ def display_info(**kwargs: Any) -> None:
 
 display_info(name="Sarmad", age=20, location="Asia")
 
+# Specify the exact structure using Dict
+user_data: Dict[str, Any] = {
+    "username": "sarmad123",
+    "email": "sarmad@example.com",
+    "age": 25,
+}
+
 
 # Unpacking dictionaries
 def create_user(username: str, email: str, age: int) -> None:
+    """
+    Unpacking dictionary
+    """
     # Function expects three keyword arguments
     print(f"Username: {username}, Email: {email}, Age: {age}")
 
 
-user_data = {"username": "sarmad123", "email": "sarmad@example.com", "age": 25}
 create_user(**user_data)  # Unpacking the dictionary into keyword arguments
 
 
 # Mixed positional and keyword arguments
 def process_data(a: int, b: int, *args: int, **kwargs: Any) -> None:
+    """
+    Mixed positional (`*args`) and keyword arguments (`**kwargs`)
+    """
     # a and b are regular positional arguments
     # *args captures additional positional arguments
     # **kwargs captures additional keyword arguments
