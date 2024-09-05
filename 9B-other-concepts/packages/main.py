77225fac67ee971924e46661930626c320d2c941
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-09-05 12:17:05 +0500

    Refactored code

--- 9B-other-concepts/packages/main.py
+++ 9B-other-concepts/packages/main.py
@@ -1,13 +1,16 @@
 """
 Using python packages
 """
+
 from write_user_to_csv import write_user_info, get_user_input
 
 
 def main():
+    """Main function to execute the package code"""
     name, email = get_user_input()
     user_info = [name, 20, email]  # Assuming age is fixed for this example
     write_user_info("user_info.csv", user_info)
+
 
 if __name__ == "__main__":
     main()
