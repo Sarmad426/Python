4260f7e133124f20e78c90f11bc7ec7a1dfbf451
Sarmad rafique <sarmadrafique040@gmail.com>
2024-09-15 23:09:16 +0500

    Refactor code to handle connection errors in API call

--- 5-Libraries/api_call.py
+++ 5-Libraries/api_call.py
@@ -4,12 +4,14 @@ Calls Simple FastAPI api using requests library
 
 import requests
 
+from requests.exceptions import RequestException
+
 # First run the FastAPI app using uvicorn [`uvicorn main:app --reload`]
 
 
 def greet_user(name: str):
     """
-    Calls FastAPI api
+    Calls FastAPI api. Greets user with name
 
     Parameters:
     name (str): Name to greet
@@ -17,15 +19,18 @@ def greet_user(name: str):
     Returns:
     (json object): Greet message
     """
-    API_URL = f"http://127.0.0.1:8000/{name}"
-    response = requests.get(API_URL)
-    msg = response.json()
-    if response.status_code == 200:
-        return msg
-    return None
+    try:
+        api_url = f"http://127.0.0.1:8000/{name}"
+        response = requests.get(api_url, timeout=5)
+        msg = response.json()
+        if response.status_code == 200:
+            return msg
+    except RequestException as req_err:
+        print(f"Connection error: {req_err}")
+        return None
 
 
-greet_name = input("Greet user with name: ")
+greet_name = input("Greet user with name: ").strip()
 
 greet_msg = greet_user(greet_name)
 
