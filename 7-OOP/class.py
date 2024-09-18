07dc3b73ac1ad277ce6780812a2eba239097e14f
Sarmad rafique <sarmadrafique040@gmail.com>
2024-09-18 15:58:02 +0500

    Refactor: Improve code and documentation for Person class

--- 7-OOP/class.py
+++ 7-OOP/class.py
@@ -1,16 +1,27 @@
 """
 Python basic Object Oriented Program
-
-    A simple Person class
 """
 
 
 class Person:
     """
-    A Simple Person class with `name`, `email` and `age`
+    A class to represent a person.
+
+    Attributes:
+        name (str): The name of the person.
+        email (str): The email address of the person.
+        age (int): The age of the person.
     """
 
     def __init__(self, name: str, email: str, age: int):
+        """
+        Constructor for the Person class.
+
+        Parameters:
+        name (str): The name of the person.
+        email (str): The email address of the person.
+        age (int): The age of the person.
+        """
         self.name = name
         self.email = email
         self.age = age
@@ -18,21 +29,16 @@ class Person:
     def greet(self) -> None:
         """
         Greet the user by their name
-
-        Takes no parameters
         """
         print(f"Hi, {self.name}!")
 
 
 if __name__ == "__main__":
-    # Get user input for name, email, and age
+
     NAME = input("Enter your name: ")
     EMAIL = input("Enter your email: ")
-
     AGE: int = int(input("Enter your age: "))
 
-    # Create Person object
     user = Person(NAME, EMAIL, AGE)
 
-    # Greet the user
     user.greet()
