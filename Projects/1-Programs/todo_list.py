301fb633879b67e39f4414168b58af331fdf4d38
Sarmad rafique <sarmadrafique040@gmail.com>
2024-09-22 10:28:01 +0500

    todo list program

---
+++ Projects/1-Programs/todo_list.py
@@ -0,0 +1,96 @@
+"""
+Todo list program
+"""
+
+
+def print_menu():
+    """
+    Prints the menu
+    """
+    print("\nTodo List Menu:")
+    print("1. View Tasks")
+    print("2. Add a Task")
+    print("3. Remove a Task")
+    print("4. Exit")
+
+
+def get_choice():
+    """
+    Get's the user's choice
+    """
+    while True:
+        choice = input("Enter your choice: ")
+        valid_choices = ("1", "2", "3", "4")
+        if choice not in valid_choices:
+            print("Invalid choice")
+            continue
+        else:
+            return choice
+
+
+def display_tasks(tasks):
+    """
+    Displays the tasks
+    """
+    if not tasks:
+        print("No tasks in the list.")
+        return
+
+    for index, task in enumerate(tasks, start=1):
+        print(f"{index}. {task}")
+
+
+def add_task(tasks):
+    """ "
+    Adds a task to the list
+    """
+    while True:
+        task = input("Enter a new task: ").strip()
+        if len(task) != 0:
+            tasks.append(task)
+            break
+        else:
+            print("Invalid task!")
+
+
+def remove_task(tasks):
+    """
+    Removes a task from the list
+    """
+    display_tasks(tasks)
+
+    while True:
+        try:
+            task_number = int(input("Enter the task number: "))
+            if 1 <= task_number <= len(tasks):
+                tasks.pop(task_number - 1)
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid task number")
+
+
+def main():
+    """
+    Main function
+    """
+    tasks = []
+
+    while True:
+        print_menu()
+
+        choice = get_choice()
+
+        if choice == "1":
+            display_tasks(tasks)
+        elif choice == "2":
+            add_task(tasks)
+        elif choice == "3":
+            remove_task(tasks)
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
