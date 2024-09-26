297452557b92e4f008bbbfa8cf88cad61a504d19
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-09-26 08:27:48 +0500

    Dockerizing a simple python program

---
+++ Projects/Docker/app.py
@@ -0,0 +1,14 @@
+"""
+Simple Python program to dockerize
+"""
+
+
+def main():
+    """
+    Main function to print Hello World!
+    """
+    print("Hello World in Python!")
+
+
+if __name__ == "__main__":
+    main()
