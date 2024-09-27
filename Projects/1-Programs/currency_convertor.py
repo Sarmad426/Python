f51ae7a4f589711831b888c7fc2d77878b7021b5
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-09-27 09:53:59 +0500

    Refactor code: Added types, updated currencies

--- Projects/1-Programs/currency_convertor.py
+++ Projects/1-Programs/currency_convertor.py
@@ -2,57 +2,67 @@
 Currency Convertor
 """
 
+from typing import Literal
 
-def get_amount():
-    """Returns the amount"""
+CURRENCIES: tuple[str, str, str] = ("PKR", "USD", "IND")
+
+TypeCurrency = Literal["Source", "Target"]
+
+EXCHANGE_RATES: dict[str, dict[str, float]] = {
+    "USD": {"PKR": 0.288, "IND": 0.48},
+    "PKR": {"USD": 2.88, "IND": 1.48},
+    "IND": {"PKR": 0.8, "USD": 0.2},
+}
+
+
+def get_amount() -> float:
+    """
+    Gets the amount from user, validates it and then returns it
+    """
     while True:
         try:
-            amount = float(input("Enter the amount: "))
-            if amount <= 0:
+            amount = input("Enter amount: ")
+            if float(amount) <= 0:
                 raise ValueError()
-            return amount
+            return float(amount)
         except ValueError:
-            print("Invalid amount")
+            print("Invalid amount! ")
 
 
-def get_currency(label):
+def get_currency(label: TypeCurrency) -> str:
     """
-    Returns the currency
+    Gets the currency from user, validates it and then returns it
     """
-    currencies = ("USD", "EUR", "CAD")
     while True:
-        currency = input(f"{label} currency (USD/EUR/CAD): ").upper()
-        if currency not in currencies:
-            print("Invalid currency")
+
+        currency = input(f"{label} currency (USD/PKR/IND): ").upper()
+        if currency not in CURRENCIES:
+            print("Invalid currency!")
         else:
             return currency
 
 
-def convert(amount, source_currency, target_currency):
+def convert(amount: float, source: str, target: str) -> float:
     """
-    Converts the amount from source currency to target currency
+    Converts source currency amount to target currency amount
     """
-    exchange_rates = {
-        "USD": {"EUR": 0.85, "CAD": 1.25},
-        "EUR": {"USD": 1.18, "CAD": 1.47},
-        "CAD": {"USD": 0.80, "EUR": 0.68},
-    }
-
-    if source_currency == target_currency:
+    if source == target:
         return amount
 
-    return amount * exchange_rates[source_currency][target_currency]
+    return amount * EXCHANGE_RATES[source][target]
 
 
 def main():
     """
-    main function
+    Main function
     """
     amount = get_amount()
     source_currency = get_currency("Source")
     target_currency = get_currency("Target")
-    converted_amount = convert(amount, source_currency, target_currency)
-    print(f"{amount} {source_currency} is equal to {converted_amount:.2f}")
+
+    conversion = convert(amount, source_currency, target_currency)
+
+    print(f"{source_currency} {amount} = {target_currency} {conversion}")
 
 
 if __name__ == "__main__":
