2e013f9b7bf04a4d4da9ef813f65c83bc2e37dbc
Sarmad rafique <sarmadrafique040@gmail.com>
2024-10-03 19:16:43 +0500

    Optimized the code

--- 6-File-IO/exercise.py
+++ 6-File-IO/exercise.py
@@ -31,16 +31,9 @@ def write_file(name: str) -> None:
         file.write(name)
 
 
-# Reading the name from the file
-
-
 def read_file() -> str:
     """
-    Reads the name from the file
-
-    Takes no parameters
-
-    returns the contents of the file
+    Returns the contents of the file
     """
     with open("practice.txt", "r", encoding="utf-8") as file:
         return file.read()
@@ -50,16 +43,15 @@ def greet_user(name: str) -> None:
     """
     Greets the user
 
-    Parameters (1) name of type string \n
-    name : str
+    Args:
+        name (str)
     """
     print(f"Welcome, {name}")
 
 
 def main():
     """
-    Main function to execute all the functions.\n
-    It doesn't take any parameters neither returns anything
+    Main function to execute all the functions.
     """
     name = get_input_name()
     validate_input(name)
